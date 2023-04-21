bd521189ddc102d2e01ed1dd088a4e0e066c1426
Eric Moore <37810345+emoore36@users.noreply.github.com>
2023-04-21 00:58:37 -0700

    Updated models to convert Entity counterparts.

--- models/Card.js
+++ models/Card.js
@@ -7,6 +7,20 @@ class Card {
         this.deck_id = deck_id;
     }
 
+    static of = (props) => {
+        const id = props.id;
+        const front_text = props.front_text;
+        const back_text = props.back_text;
+        const deck_id = props.deck_id;
+
+        if (!front_text || !back_text || !deck_id) {
+            throw new Error("Unable to create Card object from", props);
+        } else {
+            return new Card(id, front_text, back_text, deck_id);
+        }
+
+    }
+
     toString = () => {
         return `Card[id=${id},front_text=${front_text},back_text=${back_text},deck_id=${this.deck_id}]`
     }
--- models/Deck.js
+++ models/Deck.js
@@ -1,12 +1,26 @@
 class Deck {
 
-    constructor(id, name) {
+    constructor(id, name, cards=[]) {
         this.id = id;
         this.name = name;
+        this.cards = cards;
+    }
+
+    static of = (props) => {
+        const id = props.id;
+        const name = props.name;
+        const cards = props.cards || [];
+
+        if (!name) {
+            throw new Error("Unable to create Deck object from", props);
+        } else {
+            return new Deck(id, name, cards);
+        }
+
     }
 
     toString = () => {
-        return `Deck[id=${id},name=${name}]`
+        return `Deck[id=${id},name=${name},cards=${cards}]`
     }
 
 }
