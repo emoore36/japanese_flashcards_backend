9816bd2ae29a3d0dd9a1ef9c18aacf18bebc8c42
Eric Moore <37810345+emoore36@users.noreply.github.com>
2023-04-24 19:49:24 -0700

    Replaced JS object with DTO class instance.

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,11 @@
 var express = require('express');
+const DTO = require('../models/DTO');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-
-  const dto = {
-    status: 200,
-    message: 'Welcome to the Japanese Flashcards Backend server.',
-    data: {}
-  };
-
-  res.json(dto);
+router.get('/', function (req, res) {
+    const dto = new DTO(200, 'Welcome to the Japanese Flashcards Backend server.');
+    res.json(dto);
 });
 
 module.exports = router;
