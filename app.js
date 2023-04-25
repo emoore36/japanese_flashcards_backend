e6838b7fd3aa5798910215a4ae5b2eb8758f4775
Eric Moore <37810345+emoore36@users.noreply.github.com>
2023-04-24 19:48:12 -0700

    Removed frontend resources and bunyan logger.

--- app.js
+++ app.js
@@ -11,15 +11,10 @@ var decksRouter = require('./routes/decks');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors({
   origin: '*'
