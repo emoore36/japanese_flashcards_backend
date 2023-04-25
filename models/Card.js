d6701c62e570333c5543a3cd3aa0e87309fbf6b1
Eric Moore <37810345+emoore36@users.noreply.github.com>
2023-04-24 19:54:57 -0700

    Removed deck_id from Card.js

--- models/Card.js
+++ models/Card.js
@@ -1,28 +1,26 @@
 class Card {
 
-    constructor(id, front_text, back_text, deck_id) {
+    constructor(id, front_text, back_text) {
         this.id = id;
         this.front_text = front_text;
         this.back_text = back_text;
-        this.deck_id = deck_id;
     }
 
     static of = (props) => {
         const id = props.id;
         const front_text = props.front_text;
         const back_text = props.back_text;
-        const deck_id = props.deck_id;
 
-        if (!front_text || !back_text || !deck_id) {
+        if (!front_text || !back_text) {
             throw new Error("Unable to create Card object from", props);
         } else {
-            return new Card(id, front_text, back_text, deck_id);
+            return new Card(id, front_text, back_text);
         }
 
     }
 
     toString = () => {
-        return `Card[id=${id},front_text=${front_text},back_text=${back_text},deck_id=${this.deck_id}]`
+        return `Card[id=${id},front_text=${front_text},back_text=${back_text}]`
     }
 
 }
