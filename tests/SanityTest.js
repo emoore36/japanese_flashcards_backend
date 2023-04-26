c4dc8d9f0e2b0aab179b3eb7927be29fd78c2628
Eric Moore <37810345+emoore36@users.noreply.github.com>
2023-04-26 06:40:50 -0700

    Added sanity test.

---
+++ tests/SanityTest.js
@@ -0,0 +1,11 @@
+// Import the necessary modules and functions for testing
+const { expect } = require('chai');
+
+// Describe the test
+describe('Sanity check', () => {
+    // Write the test
+    it('Should always pass', () => {
+        // Perform the test
+        expect(true).to.equal(true);
+    });
+});
